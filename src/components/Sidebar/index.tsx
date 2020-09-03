bc26ec30189f1131e584203cace4e2e57ce2ccee
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-09-03 17:27:28 -0300

    Inicial Struture from Sidebar 🔥

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container, SearchWrapper, SearchInput, SearchIcon, Body } from './styles';
+
+const Sidebar: React.FC = () => {
+  return (
+    <Container>
+        <SearchWrapper>
+            <SearchInput placeholder='Buscar no Twitter' />
+            <SearchIcon />
+        </SearchWrapper>
+        <Body>
+            <p> { 'lorem ipsum dolor sit amet. ' . repeat(90) } </p>
+        </Body>
+    </Container>
+  );
+};
+
+export default Sidebar;
